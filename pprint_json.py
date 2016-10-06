18b0c1949d78145a3c713c126d6efcc82d3db1c9
Nikita Tkachenko <ybrbnf418@gmail.com>
2016-10-06 09:16:06 +0300

    v 1.0

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,24 @@
+
 import json
 
 
 def load_data(filepath):
-    pass
+    return json.load(filepath)
 
 
 def pretty_print_json(data):
-    pass
+    pprint_json = json.dumps(data, indent=4, sort_keys=True)
+    return pprint_json
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        filepath = input('Вседите путь и имя файла: ')
+        with open(filepath, 'r') as file_:
+            data = load_data(file_)
+
+    except FileNotFoundError:
+        print ('Нет такого файла или папки. Программа будет закрыта.')
+        exit()
+    pprint_json = pretty_print_json(data)
+    print (pprint_json)
